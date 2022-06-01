f905b28a386ee76acdb47069be4b8d9ec7570e45
Cdanti <chrisdanti2001@gmail.com>
2022-06-01 16:43:16 -0400

    finished sections 1, 2, and 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = '';
+    let stringArray = string.split("");
+    if (string.length > 0){
+        for (let i=0; i <= string.length-1; i++){
+            result = result + stringArray.at(-1);
+            stringArray.pop();
+        }
+    }else{
+        result = '';
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
