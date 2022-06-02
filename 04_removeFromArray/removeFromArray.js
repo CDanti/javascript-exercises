b4be0fd65a4d37978bcc73b81e0dc8889334acd8
Cdanti <chrisdanti2001@gmail.com>
2022-06-02 14:50:59 -0400

    finished part 4, had to get help/refer to solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, ...args) {
+    finalArray = []
+    myArray.forEach(item => {
+        if (!args.includes(item)){
+            finalArray.push(item)
+        }
+    })
+    return finalArray
 };
+
+//if (myArray.includes(num)){
+    //     myArray.splice(myArray.indexOf(num),1)
+    // }
+    // else{
+    //     myArray = myArray
+    // }
 
 // Do not edit below this line
 module.exports = removeFromArray;
