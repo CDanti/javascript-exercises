a55a3b35d020e81c076673a8807ba7d36668a9f6
Cdanti <chrisdanti2001@gmail.com>
2022-06-03 16:14:36 -0400

    finished section 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  c_temp = (temp - 32)*(5/9)
+  c_final = Math.round(c_temp * 10) / 10
+  return c_final;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  f_temp = (temp * (9/5))+32
+  f_final = Math.round(f_temp * 10) / 10
+  return f_final;
 };
 
 // Do not edit below this line
